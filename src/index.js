13fb77836f1e59dae0ee98de359d660c4892f0f4
afoxbr <antonyrbramwell@gmail.com>
2022-08-10 20:25:49 +0100

    Add scripting for title and icon creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("o hai");
+const title = document.createElement("h2");
+title.innerHTML = "Welcome to Chris' Milk Shack!";
+document.getElementById("content").appendChild(title);
+
+const icon = document.createElement("img");
+icon.setAttribute("src", "../src/assets/20220810_201741_0000.png");
+document.getElementById("content").appendChild(icon);
