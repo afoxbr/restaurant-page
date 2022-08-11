626d14b43d1c9247cca979172b982dfbe9ab342e
afoxbr <antonyrbramwell@gmail.com>
2022-08-11 21:09:50 +0100

    Create content script pages

---
+++ src/about.js
@@ -0,0 +1,20 @@
+const aboutContent = () => {
+    //This function adds the about page content
+    const title = document.createElement("h2");
+    title.innerHTML = "Welcome to Chris' Milk Shack!";
+    document.getElementById("content").appendChild(title);
+
+    const icon = document.createElement("img");
+    icon.src="../src/assets/20220810_201741_0000.png";
+    icon.setAttribute("height", "480px");
+    icon.setAttribute("width", "480px");
+    document.getElementById("content").appendChild(icon);
+
+    const blurb = document.createElement("p");
+    blurb.innerText = "Chris' Milk Shack is Canada's premium outlet for mammal juice of all varieties! Cow? You bet! Goat? Of course! Human? As long as you don't ask where we got it!";
+    document.getElementById("content").appendChild(blurb);
+};
+
+export {
+    aboutContent,
+};
