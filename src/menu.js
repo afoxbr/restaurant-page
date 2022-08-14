883d8ab244dc34b278106d2ca9e8039af2ec2923
afoxbr <antonyrbramwell@gmail.com>
2022-08-14 17:28:12 +0100

    Add menu function, add buttons to index.js

---
+++ src/menu.js
@@ -0,0 +1,34 @@
+const menuContent = () => {
+    //Adds menu content
+    const menuTitle = document.createElement("h3");
+    menuTitle.innerHTML = "Menu";
+    document.getElementById("content").appendChild(menuTitle);
+
+    const basicMilk = document.createElement("div");
+    basicMilk.classList.add("recipeCard");
+    document.getElementById("content").appendChild(basicMilk);
+
+    const basicMilkTitle = document.createElement("h4");
+    basicMilkTitle.innerHTML = "Milk";
+    basicMilk.appendChild(basicMilkTitle);
+
+    const basicBlurb = document.createElement("p");
+    basicBlurb.innerHTML = "Unflavoured milk from cow, goat, or surprise mammal.\n$45";
+    basicMilk.appendChild(basicBlurb);
+
+    const flavouredMilk = document.createElement("div");
+    flavouredMilk.classList.add("recipeCard");
+    document.getElementById("content");
+
+    const flavouredMilkTitle = document.createElement("h4");
+    flavouredMilkTitle.innerHTML = "Flavoured Milk";
+    flavouredMilk.appendChild(flavouredMilkTitle);
+
+    const flavouredBlurb = document.createElement("p");
+    flavouredBlurb.innerHTML = "As above, but flavoured with chocolate, strawberry, poutine, or moose.\n$65"
+    flavouredMilk.appendChild(flavouredBlurb);
+};
+
+export {
+    menuContent
+};
