2680678687f54ebc109e56cd706e3fbc2da9ecce
afoxbr <antonyrbramwell@gmail.com>
2022-08-17 22:11:48 +0100

    Add styling

--- src/contact.js
+++ src/contact.js
@@ -11,15 +11,15 @@ function contactContent() {
     main.appendChild(details);
 
     const address = document.createElement("li");
-    address.innerHTML = "Address:<br>8140 Leslie Rd<br>Richmond<br>British Columbia<br>V6X 4A8<br>Canada";
+    address.innerHTML = "<b>Address</b><br>8140 Leslie Rd<br>Richmond<br>British Columbia<br>V6X 4A8<br>Canada";
     details.appendChild(address);
 
     const phone = document.createElement("li");
-    phone.innerHTML = "Phone number<br>(604) 276-8282";
+    phone.innerHTML = "<b>Phone number</b><br>(604) 276-8282";
     details.appendChild(phone);
 
     const email = document.createElement("li");
-    email.innerHTML = "Email<br>chris@milkshack.com";
+    email.innerHTML = "<b>Email</b><br>chris@milkshack.com";
     details.appendChild(email);
 }
 
